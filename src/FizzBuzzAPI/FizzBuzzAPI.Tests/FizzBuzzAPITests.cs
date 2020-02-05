a175227444e5e09fd3986495a6ec929198fd69b4
Ben Burton <ben.burton@outlook.com>
2020-02-05 20:07:49 +0000

    Extend tests to cover single and range non multiple of 3 and 5

--- src/FizzBuzzAPI/FizzBuzzAPI.Tests/FizzBuzzAPITests.cs
+++ src/FizzBuzzAPI/FizzBuzzAPI.Tests/FizzBuzzAPITests.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace FizzBuzzAPI.Tests
 {
     [TestFixture]
-    public class GivenARangeWithANonMultipleOfEither3Or5 : WebApplicationTestBase
+    public class GivenASingleNonMultipleOfEither3Or5 : WebApplicationTestBase
     {
         private ExpectedResponse _expectedResponse;
 
@@ -21,6 +21,26 @@ namespace FizzBuzzAPI.Tests
         public void ThenTheResultIsNumber()
         {
             Assert.That("2", Is.EqualTo(_expectedResponse.Result));
+        }
+    }
+
+    [TestFixture]
+    public class GivenARangeWithANonMultipleOfEither3Or5 : WebApplicationTestBase
+    {
+        private ExpectedResponse _expectedResponse;
+
+        [OneTimeSetUp]
+        public async Task WhenTheAPIIsCalledWithRange()
+        {
+            var response = await Client.GetAsync($"{FizzBuzzRoute}/1/2");
+
+            _expectedResponse = JsonConvert.DeserializeObject<ExpectedResponse>(await response.Content.ReadAsStringAsync());
+        }
+
+        [Test]
+        public void ThenTheResultIsNumber()
+        {
+            Assert.That("1 2", Is.EqualTo(_expectedResponse.Result));
         }
     }
 }
